7966cbd914e9da56c8fe291b26d3aa6bcc5897f6
ThomasRief <robertclara@web>
2015-06-29 14:00:18 +0200

    einige Aenderungen

--- hallo.c
+++ hallo.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <stdio.h>
-	
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
-	FILE * fp
+	FILE * fp;
 	if(argc != 2)
 	{
 		printf("ERROR\n");
-		return EXIT_FAILURE; 
+		return EXIT_FAILURE;
 	}
-	char dateiname[20], content[135]; 
-	strncpy(dateiname, argv[1], 20); 
-	fp = fopen("dateiname" "r");
+	char dateiname[20], content[135];
+	strncpy(dateiname, argv[1], 20);
+	fp = fopen(dateiname, "r");
 	if(fp==NULL)
-	{ 
-		printf("ERROR! File could not be opened.\n"); 
-		return EXIT_FAILURE; 
-	}
-	while(feof==0)
 	{
-		fgets(content, 135, fp); 
-		printf("%s", content); 
+		printf("ERROR! File could not be opened.\n");
+		return EXIT_FAILURE;
 	}
-	return EXIT_SUCCESS; 
+	while(feof(fp)==0)
+	{
+		fgets(content, 135, fp);
+		printf("%s", content);
+	}
+	return EXIT_SUCCESS;
 }
